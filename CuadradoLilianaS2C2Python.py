30a60de0cbb458c76519623134ca0957b04f710d
lmcuadrado <42412749+lmcuadrado@users.noreply.github.com>
2018-09-07 09:08:54 -0500

    Add files via upload

---
+++ CuadradoLilianaS2C2Python.py
@@ -0,0 +1,32 @@
+# Ejercicio 1
+# Complete el siguiente codigo para que:
+# recorra la lista `x` 
+# imprima los numeros impares 
+# y pare de imprimir al encontrar un numero mayor a 800
+
+
+
+
+#import numpy as np
+#x = np.int_(np.random.random(100)*900)
+# x=[12, 34, 57, 789, 842, 17, 843, 22, 21] 
+#print(x)
+
+
+
+
+# Ejercicio 2
+#Haga una funcion llamada division que reciba como argumentos dos variables a y b y retorne a/b
+
+def division(a,b):
+ 		c=a/b
+		print c
+
+#si "descomenta" la siguiente linea, su codigo deberia imprimir 4.5
+#print( division(9, 2) )
+
+
+
+# Ejercicio 3
+
+
